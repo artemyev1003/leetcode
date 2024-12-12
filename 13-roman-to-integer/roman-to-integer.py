52d686771815bb591f543d6f6e842f6c031561c2
artemyev1003 <102872314+artemyev1003@users.noreply.github.com>
2024-12-13 00:33:20 +0300

    Time: 11 ms (16.31%) | Memory: 17.3 MB (9.46%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -2,27 +2,20 @@ class Solution:
     def romanToInt(self, s: str) -> int:
         num_mapping = {
             "I": 1,
-            "IV": 4,
             "V": 5,
-            "IX": 9,
             "X": 10,
-            "XL": 40,
             "L": 50,
-            "XC": 90,
             "C": 100,
-            "CD": 400,
             "D": 500,
-            "CM": 900,
             "M": 1000,
         }
 
         result = 0
-        i = 0
-        while i < len(s):
-            if (n := s[i:i+2]) in num_mapping:
-                result += num_mapping[n]
-                i += 2
+        length = len(s)
+        for i in range(length):
+            curr = num_mapping[s[i]]
+            if i < length - 1 and curr < num_mapping[s[i + 1]]:
+                result -= curr
             else:
-                result += num_mapping[s[i]]
-                i += 1
+                result += curr
         return result
