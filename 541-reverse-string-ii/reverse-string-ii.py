49a9b4b8f0b4a5bf48ea795dd8989ed601ab4610
artemyev1003 <102872314+artemyev1003@users.noreply.github.com>
2025-02-16 16:45:03 +0300

    Time: 4 ms (16.95%) | Memory: 17.9 MB (49.09%) - LeetSync

--- 541-reverse-string-ii/reverse-string-ii.py
+++ 541-reverse-string-ii/reverse-string-ii.py
@@ -5,29 +5,16 @@ class Solution:
             if (i + 1) % (2 * k) == 0:
                 r = i - k
                 l = r - k + 1
-                print("l = ", l)
-                print("r = ", r)
                 self._reverse(list_s, l, r)
-                print("list_s = ", list_s)
-        if (rem := len(list_s) % (2 * k)) > 0:
-            
-            print("rem = ", rem)
+        if (rem := len(list_s) % (2 * k)) > 0:            
             if rem < k:
-                print("wo")
                 l = i - rem + 1
                 r = i
-                print("l = ", l)
-                print("r = ", r)
                 self._reverse(list_s, l, r)
-                print("list_s = ", list_s)
             else:
-                print("here")
                 l = i - rem + 1 
                 r = l + k - 1
-                print("l = ", l)
-                print("r = ", r)
                 self._reverse(list_s, l, r)
-                print("list_s = ", list_s)
         return "".join(list_s)
     
     def _reverse(self, arr: list[int], l: int, r: int) -> None:
